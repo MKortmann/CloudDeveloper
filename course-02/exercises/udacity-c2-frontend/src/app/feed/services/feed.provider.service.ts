df3587680395d5dfb0ff9f00f6a6569c788ab43b
MKortmann <companymvms@gmail.com>
2021-02-04 10:45:56 +0100

    [udacity-c2-frontend] - updating environment

--- course-02/exercises/udacity-c2-frontend/src/app/feed/services/feed.provider.service.ts
+++ course-02/exercises/udacity-c2-frontend/src/app/feed/services/feed.provider.service.ts
@@ -13,6 +13,7 @@ export class FeedProviderService {
   constructor(private api: ApiService) { }
 
   async getFeed(): Promise<BehaviorSubject<FeedItem[]>> {
+    debugger
     const req = await this.api.get('/feed');
     const items = <FeedItem[]> req.rows;
     this.currentFeed$.next(items);
